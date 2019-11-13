91eecef8738b1da494cb788bd155b52f2c15b4d1
zdc <849247596@qq.com>
2019-11-13 22:03:46 +0800

    111111

--- FirstLearning/src/com/zdc/cn/Demo.java
+++ FirstLearning/src/com/zdc/cn/Demo.java
@@ -1,8 +1,14 @@
 package com.zdc.cn;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hehe");
+        List<String> list =new LinkedList<>();
+        list.add("ddd");
+
     }
 }
